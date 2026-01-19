bcc8b2376fb2560c56b5a402b38108980c1b2ba2
Nitish <nitishnbt0452@gmail.com>
2026-01-19 20:16:35 +0530

    ques no 10

---
+++ DiamondShape_Pattern.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class DiamondShape_Pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number here : ");
+        int num = sc.nextInt();
+        int space = num / 2;
+        int str = 1;
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= space; j++) {
+                System.out.print("\t");
+            }
+            for (int j = 1; j <= str; j++) {
+                System.out.print("*\t");
+            }
+            if (i <= num / 2) {
+                space--;
+                str += 2;
+            } else {
+                space++;
+                str-=2;
+            }
+            System.out.println();
+        }
+        sc.close();
+
+    }
+}
