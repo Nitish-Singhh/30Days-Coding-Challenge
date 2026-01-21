697b09430a1c0e9f7383d34b0469d29113659bd3
Nitish <nitishnbt0452@gmail.com>
2026-01-21 21:11:54 +0530

    19

---
+++ Find_Even_in_Array.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Find_Even_in_Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Even numbers in the array:");
+        for (int num : arr) {
+            if (num % 2 == 0) {
+                System.out.println(num);
+            }
+        }
+        sc.close();
+    }
+}
