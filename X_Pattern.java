db3ff5fbb5d24fd3c3e7233ceaf27f2378939ad8
Nitish <nitishnbt0452@gmail.com>
2026-01-22 19:39:51 +0530

    20

---
+++ X_Pattern.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class X_Pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number here : ");
+        int size = sc.nextInt();
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (i == j || i + j == size + 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
