ca65634e411b04a668a163c2cfec3d28e473fb76
Nitish <nitishnbt0452@gmail.com>
2026-01-22 19:40:55 +0530

    20

--- X_Pattern.java
+++ X_Pattern.java
@@ -6,7 +6,7 @@ public class X_Pattern {
         System.out.println("Enter a Number here : ");
         int size = sc.nextInt();
         for (int i = 1; i <= size; i++) {
-            for (int j = 1; j <= size; j++) {
+            for (int j = 1; j <= size; j++) { 
                 if (i == j || i + j == size + 1) {
                     System.out.print("* ");
                 } else {
