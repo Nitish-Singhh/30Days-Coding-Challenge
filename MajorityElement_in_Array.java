73eea12d07e0157373ed85dba77084d6a72979b8
Nitish <nitishnbt0452@gmail.com>
2026-01-29 21:17:28 +0530

    36

---
+++ MajorityElement_in_Array.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class MajorityElement_in_Array {
+    public static int findMajorityElement(int[] arr) {
+        int count = 0;
+        Integer candidate = null;
+        for (int num : arr) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        count = 0;
+        for (int num : arr) {
+            if (num == candidate) {
+                count++;
+            }
+        }
+
+        return (count > arr.length / 2) ? candidate : -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int majorityElement = findMajorityElement(arr);
+        if (majorityElement != -1) {
+            System.out.println("The majority element is: " + majorityElement);
+        } else {
+            System.out.println("No majority element found.");
+        }
+        sc.close();
+
+    }
+}
